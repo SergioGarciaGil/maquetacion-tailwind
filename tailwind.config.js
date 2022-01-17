81cf1f48f9773ac653ec96d9eb1a73dd8c9855a5
SergioGarciaGil <sergio.cuerda@gmail.com>
2022-01-16 22:33:09 -0500

    inicio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        "body-pattern": "url('../img/pattern.png')",
+      }),
+      colors: {
+        "azul-claro": "#37bcf9",
+        "azul-oscuro": "0370b9",
+      },
+    },
+  },
+  variants: {},
+};
